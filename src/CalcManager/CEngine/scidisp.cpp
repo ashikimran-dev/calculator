fbce3ebd14b5a6db5154434edfc7458ebb4a92c1
Ashik Akbar Basha <ashikimran1992@gmail.com>
2020-03-25 17:00:10 -0400

    Testing more commits

--- src/CalcManager/CEngine/scidisp.cpp
+++ src/CalcManager/CEngine/scidisp.cpp
@@ -85,6 +85,7 @@ void CCalcEngine::DisplayNum(void)
     //  called.
     //
     // Starting point here
+    // Testing new commit
     if (m_bRecord || gldPrevious.value != m_currentVal || gldPrevious.precision != m_precision || gldPrevious.radix != m_radix || gldPrevious.nFE != (int)m_nFE
         || gldPrevious.bUseSep != true || gldPrevious.numwidth != m_numwidth || gldPrevious.fIntMath != m_fIntegerMode || gldPrevious.bRecord != m_bRecord)
     {
